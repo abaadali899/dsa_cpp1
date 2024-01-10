c1f01f77f2e00ba376aa938c843ca28d128c3a18
Abaad Ali <93701613+abaadali899@users.noreply.github.com>
2024-01-11 01:58:03 +0500

    Create loops1.cpp
    master dsa

---
+++ loops1.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+    cout<<"sum of n numbers" << endl;
+    int n=6;
+    int sum=0;
+    int i;
+    while(i<=6){
+    sum=sum+i;
+    
+    i++;
+    }
+    cout<<sum<<endl;
+    return 0;
+}
